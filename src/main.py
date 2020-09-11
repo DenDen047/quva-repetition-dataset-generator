2751f6bd25e73fc5b59b5ad5c008076438a1c38a
DenDen047 <sh.mn.nat@gmail.com>
2020-09-11 19:16:13 +0900

    load npy file

--- src/main.py
+++ src/main.py
@@ -5,18 +5,17 @@ import time
 import pathlib
 import argparse
 import logging
+import glob
 from tqdm import tqdm
 import cv2 as cv
 import numpy as np
 from pprint import pprint
 
-import youtube_dl
-
 
 # setting for argparse
 parser = argparse.ArgumentParser(description='Analysis an image showing company structure')
-parser.add_argument('--dataset_dir', type=str, default='/data', help='the directory path where the countix csv files and outputs are')
-parser.add_argument('--reshape_video', action='store_true', default=False, help='to save the space, the downloaded videos are reshaped into 224x224')
+parser.add_argument('--quva_data_dir', type=str, default='/data/QUVARepetitionDataset', help='the directory path where the QUVA Repetition Dataset are put')
+parser.add_argument('--output_dir', type=str, default='/data/output', help='the directory path where the outputs are put')
 parser.add_argument('--logging', action='store_true', default=False, help='if true, output the all image/pdf files during the process')
 
 args = parser.parse_args()
@@ -35,27 +34,6 @@ logger = logging.getLogger(__name__)
 _console_handler = logging.StreamHandler(sys.stdout)
 _console_handler.setLevel(logging.DEBUG)
 logger.addHandler(_console_handler)
-
-
-def download_video_from_url(
-    url_to_video: str,
-    path_to_video: str,
-    skip_existing_videos: bool = True
-) -> str:
-    """ This function is copied from https://colab.research.google.com/github/google-research/google-research/blob/master/repnet/repnet_colab.ipynb """
-
-    if os.path.exists(path_to_video):
-        if skip_existing_videos:
-            return 'skipped'
-        else:
-            os.remove(path_to_video)
-    ydl_opts = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
-        'outtmpl': str(path_to_video),
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url_to_video])
-        return 'success'
 
 
 def read_video(
@@ -84,67 +62,17 @@ def read_video(
 
 
 if __name__ == "__main__":
-    data_dir = pathlib.Path(args.dataset_dir).resolve()
-    video_dir = os.path.join(data_dir, 'downloaded_videos')
-    npz_dir = os.path.join(data_dir, 'video_frame_data')
-    csv_file_names = [
-        'countix_train.csv',
-        'countix_val.csv',
-        'countix_test.csv'
-    ]
-
-    os.makedirs(data_dir, exist_ok=True)
-    os.makedirs(video_dir, exist_ok=True)
-    os.makedirs(npz_dir, exist_ok=True)
+    quva_dir = pathlib.Path(args.quva_data_dir).resolve()
+    video_dir = os.path.join(quva_dir, 'videos')
+    npy_dir = os.path.join(quva_dir, 'annotations')
 
     logger.info(f'command line arguments: {args}')
 
-    for csv_file_name in csv_file_names:
-        logger.info(f'load {csv_file_name}')
+    npy_fpaths = glob.glob(os.path.join(npy_dir, '*.npy'))
 
-        with open(os.path.join(data_dir, csv_file_name)) as csv_file:
-            csv_reader = csv.DictReader(csv_file)
+    for npy_fpath in npy_fpaths:
+        pprint(npy_fpath)
+        result = np.load(npy_fpath)
+        pprint(result)
 
-            for row in csv_reader:
-                url = 'http://youtube.com/watch?v={}'.format(row['video_id'])
-                logger.debug(url)
-
-                # check if the video was already downloaded
-                video_fpath = os.path.join(video_dir, f"{row['video_id']}.mp4")
-                if os.path.exists(video_fpath):
-                    continue
-
-                # download
-                try:
-                    r = download_video_from_url(
-                        url_to_video=url,
-                        path_to_video=video_fpath
-                    )
-                except Exception as e:
-                    logger.debug(e)
-
-                # reshape the video
-                if args.reshape_video:
-                    imgs, vid_info = read_video(video_fpath)
-                    n_frames, w, h, c = imgs.shape
-
-                    def t2f(time):  # time to frame
-                        return time * n_frames / vid_info['time']
-
-                    rep_start = float(row['repetition_start'])
-                    rep_end = float(row['repetition_end'])
-                    n_rep = float(row['count'])
-
-                    period_length = t2f(rep_end - rep_start) / n_rep
-
-                    periodicities = np.zeros((n_frames,))
-                    periodicities[
-                        round(t2f(rep_start)):round(t2f(rep_end))+1
-                    ] = 1
-
-                    # save data & remove the video
-                    np.savez_compressed(
-                        os.path.join(npz_dir, row['video_id']),
-                        imgs, periodicities, period_length
-                    )
-                    os.remove(video_fpath)
+        break
